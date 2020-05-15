0200b0abfce23e1e59629e56ab616f636cbdaa25
arondasamuel123 <arondasamuel123@gmail.com>
2020-05-15 17:02:54 +0300

    Added chatroom component and added a form for sending messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ChatroomComponent
   ],
   imports: [
     BrowserModule,
