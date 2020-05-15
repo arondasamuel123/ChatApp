4ae534506f6e7b9228db93220602d139a3a46700
arondasamuel123 <arondasamuel123@gmail.com>
2020-05-15 18:41:24 +0300

    Created messages component and backend service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ChatroomComponent } from './chatroom/chatroom.component';
+import { MessagesListComponent } from './messages-list/messages-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ChatroomComponent
+    ChatroomComponent,
+    MessagesListComponent
   ],
   imports: [
     BrowserModule,
